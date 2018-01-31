f91d3d12a83185760cfab48be3825d2abaeb6913
James Taylor <james.taylor@kingsmensoftware.com>
2018-01-31 15:58:15 -0500

    firebase config refactor, moved all of firebase config and init out app index

--- src/containers/App/firebase-config.js
+++ src/containers/App/firebase-config.js
@@ -1,8 +1,18 @@
-export default {
-  apiKey: 'AIzaSyBeGhpMXGz_1yfYjwYDd0JHRG0a6yHLve4',
-  authDomain: 'reddit-clone-4186c.firebaseapp.com',
-  databaseURL: 'https://reddit-clone-4186c.firebaseio.com',
-  projectId: 'reddit-clone-4186c',
-  storageBucket: 'reddit-clone-4186c.appspot.com',
-  messagingSenderId: '996838905727',
+import firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyBeGhpMXGz_1yfYjwYDd0JHRG0a6yHLve4",
+  authDomain: "reddit-clone-4186c.firebaseapp.com",
+  databaseURL: "https://reddit-clone-4186c.firebaseio.com",
+  messagingSenderId: "996838905727",
+  projectId: "reddit-clone-4186c",
+  storageBucket: "reddit-clone-4186c.appspot.com"
 };
+
+firebase.initializeApp(config);
+
+export default firebase;
+
+export const database = firebase.database();
+// export const auth = firebase.auth();
+// export const googleAuthProvider = new firebase.authGoogleAuthProvider();
--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,50 +1,41 @@
-import React, { Component } from 'react';
-import * as firebase from 'firebase';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import firebase, { database } from "./firebase-config";
+import styled from "styled-components";
 
-import Routes from '../../routes';
-import config from './firebase-config';
+import Routes from "../../routes";
+import config from "./firebase-config";
 
 class App extends Component {
   constructor() {
     super();
 
-    // Initialize Firebase
-    firebase.initializeApp(config);
-
     this.state = {
       posts: {},
       // loading: false,
-      firebase: null,
+      firebase: null
     };
   }
 
   componentWillMount() {
     // eslint-disable-next-line prefer-const
-    let postsRef = firebase.database().ref('posts');
+    let postsRef = database.ref("posts");
     // eslint-disable-next-line prefer-const
     let self = this;
 
-    postsRef.on('value', (snapshot) => {
+    postsRef.on("value", snapshot => {
       self.setState({
         posts: snapshot.val(),
         // loading: false,
-        firebase: firebase.database(),
+        firebase: database
       });
     });
   }
 
   render() {
-    const AppContainer = styled.div`
-      // height: 100%;
-      // width: 100%;
-      // display: flex;
-    `;
-    // console.log(`app: ${this.state.posts}`);
     return (
-      <AppContainer className="app-container">
+      <div className="app-container">
         <Routes firebase={this.state.firebase} posts={this.state.posts} />
-      </AppContainer>
+      </div>
     );
   }
 }
