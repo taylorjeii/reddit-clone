145dd98055165baf7e9e0d357290f2cb6d57235e
James Taylor <james.taylor@kingsmensoftware.com>
2018-02-14 16:11:26 -0500

    added routes to app component, converted app components to stateless

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,31 +1,26 @@
 import React, { Component } from 'react';
 import { db, firebase } from '../../firebase';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import styled from 'styled-components';
 
-import Routes from '../../routes';
+import Posts from '../Posts';
+import AddPost from '../AddPost';
+import Navigation from '../../components/Navigation';
 
-class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      firebase: null
-    };
-  }
-
-  componentWillMount() {
-    this.setState({
-      firebase: db.getDatabase()
-    });
-  }
-
-  render() {
-    return (
-      <div className="app-container">
-        <Routes firebase={this.state.firebase} />
+const App = () => (
+  <div className="app-container">
+    <Router>
+      <div>
+        <Navigation />
+        <Switch>
+          <Redirect exact from="/" to="/posts" />
+          <Route exact path="/posts" component={Posts} />
+          <Route exact path="/addpost" component={AddPost} />
+          <Route component={() => <h1>NotFound</h1>} />
+        </Switch>
       </div>
-    );
-  }
-}
+    </Router>
+  </div>
+);
 
 export default App;
