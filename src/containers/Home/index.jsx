59842a9a4d7c3a02229af1bb92863fa582a40efa
James Taylor <james.taylor@kingsmensoftware.com>
2018-02-21 15:39:33 -0500

    home page styling

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 90px);
+`;
+
+const Title = styled.h1`
+  color: #fff;
+  font-size: 5rem;
+`;
+
 const App = () => (
-  <div>
-    <h1>Landing Page</h1>
-  </div>
+  <Wrapper>
+    <Title>Reddit Clone</Title>
+  </Wrapper>
 );
 
 export default App;
