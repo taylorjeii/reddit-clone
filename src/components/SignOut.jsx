2532d3799f197efb513c61817f1b24d85921a074
James Taylor <james.taylor@kingsmensoftware.com>
2018-02-21 11:33:47 -0500

    styled sign out button, rerouted user to landing page on sign out

--- src/components/SignOut.jsx
+++ src/components/SignOut.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+import { withRouter } from 'react-router-dom';
+
 import { auth } from '../firebase';
 
-const SignOutButton = () => (
-  <button type="button" onClick={auth.doSignOut}>
-    Sign Out
-  </button>
-);
+const SignOutBtn = styled.a`
+  border: 1px solid #598234;
+  background: #598234;
+  color: #fff;
+  font-family: 'Roboto Condensed', sans-serif;
+  letter-spacing: 1px;
+  margin: 0 0.5rem;
+  padding: 0.5rem;
+  text-decoration: none;
+  transition: all 0.2s ease-out;
 
-export default SignOutButton;
+  &:hover {
+    border-color: #fff;
+    cursor: pointer;
+  }
+`;
+
+const SignOutButton = ({ history }) => {
+  const signUserOut = () => {
+    auth.doSignOut();
+    history.push('/');
+  };
+  return (
+    <SignOutBtn type="button" onClick={() => signUserOut()}>
+      Sign Out
+    </SignOutBtn>
+  );
+};
+export default withRouter(SignOutButton);
