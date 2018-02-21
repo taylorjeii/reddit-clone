f33bfa40fddc7dd3c20bb195f673c40cd1fecc84
James Taylor <james.taylor@kingsmensoftware.com>
2018-02-21 11:32:27 -0500

    added check for auth user, converted app to class component to access lifecycle hooks

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -10,22 +10,40 @@ import SignUp from '../SignUp';
 import SignIn from '../SignIn';
 import Navigation from '../../components/Navigation';
 
-const App = () => (
-  <div className="app-container">
-    <Router>
-      <div>
-        <Navigation />
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/posts" component={Posts} />
-          <Route exact path="/add-post" component={AddPost} />
-          <Route exact path="/sign-up" component={SignUp} />
-          <Route exact path="/sign-in" component={SignIn} />
-          <Route component={() => <h1>NotFound</h1>} />
-        </Switch>
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authUser: null
+    };
+  }
+
+  componentDidMount() {
+    firebase.auth.onAuthStateChanged(authUser => {
+      authUser ? this.setState(() => ({ authUser })) : this.setState(() => ({ authUser: null }));
+    });
+  }
+
+  render() {
+    return (
+      <div className="app-container">
+        <Router>
+          <div>
+            <Navigation authUser={this.state.authUser} />
+            <Switch>
+              <Route exact path="/" component={Landing} />
+              <Route exact path="/posts" component={Posts} />
+              <Route exact path="/add-post" component={AddPost} />
+              <Route exact path="/sign-up" component={SignUp} />
+              <Route exact path="/sign-in" component={SignIn} />
+              <Route component={() => <h1>NotFound</h1>} />
+            </Switch>
+          </div>
+        </Router>
       </div>
-    </Router>
-  </div>
-);
+    );
+  }
+}
 
 export default App;
